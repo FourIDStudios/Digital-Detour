d04207076c792c26105076dd83624450e9f6a668
Martineus Fearon <mcqughfearon61@gmail.com>
2025-02-16 09:22:45 -0600

    Fixed MongoDb Implementation
    Scenarios not accurately update based on scenario information saved in the database

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -14,13 +14,17 @@ app = Flask(__name__)
 
 # MongoDB Atlas connection
 print(dbPassword)
-db_uri = "mongodb+srv://Martineus:"+dbPassword+"@mbdatabase.heuls.mongodb.net/?retryWrites=true&w=majority&appName=MBDatabase"
+db_uri = "mongodb+srv://Martineus:Hacklahoma@mbdatabase.heuls.mongodb.net/?retryWrites=true&w=majority&appName=MBDatabase"
 client = MongoClient(db_uri, server_api=ServerApi('1'))
-db = client['Scenarios']
+db = client.get_database("Hacklahoma")
+collection = db.get_collection("Scenarios")
+print(collection.find())
+print(db.get_collection("Scenarios"))
+
 
 @app.route('/scenarios', methods=['GET'])
 def get_scenarios():
-    scenarios = list(db.scenarios.find({}, {'_id': 0}))  # Exclude the MongoDB _id field
+    scenarios = list(db.get_collection("Scenarios").find({}, {'_id': 0}))  # Exclude the MongoDB _id field
     return jsonify(scenarios)
 
 if __name__ == '__main__':
