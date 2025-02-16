7c7232fb9e5687ddccc082d4ff668d371180fbde
Martineus Fearon <mcqughfearon61@gmail.com>
2025-02-16 08:45:06 -0600

    Flask Server
    Experiemented with flask server to handle mongo db logic,  as more of a service. Will most likely just opt for rongo

---
+++ FlaskServer/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+#Load .Env
+load_dotenv()
+
+#Access Env Variables
+dbPassword = os.getenv("DB_PASSWORD")
+
+app = Flask(__name__)
+
+# MongoDB Atlas connection
+print(dbPassword)
+db_uri = "mongodb+srv://Martineus:"+dbPassword+"@mbdatabase.heuls.mongodb.net/?retryWrites=true&w=majority&appName=MBDatabase"
+client = MongoClient(db_uri, server_api=ServerApi('1'))
+db = client['Scenarios']
+
+@app.route('/scenarios', methods=['GET'])
+def get_scenarios():
+    scenarios = list(db.scenarios.find({}, {'_id': 0}))  # Exclude the MongoDB _id field
+    return jsonify(scenarios)
+
+if __name__ == '__main__':
+    app.run(port=5000)
